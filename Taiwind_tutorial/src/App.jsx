6a0e5fcfdaa9e3644aa77f939bff2449d314d65b
masahiro <m.togasaki1026@gmail.com>
2025-11-30 10:52:40 +1100

    First_Commit&Learned JIT concepts

---
+++ Taiwind_tutorial/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div class="my-10 flex h-10 w-full items-center justify-center rounded-md border-2 border-violet-600 bg-violet-200 p-6">
+  {/* <h1 class="text-center font-mono font-extrabold text-[30px]">Hello World !</h1> */}
+</div>
+
+// JIT Just In Time Compiler
+
+  )
+}
+
+export default App
+  
